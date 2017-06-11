aab686b5a393101366ad7cb89c0b34b66f62eeb2
ausichenko <ausichenko@crystalnix.com>
2017-06-11 13:17:29 +0600

    Fix creating fragments. Increase text size.

--- app/src/main/java/com/ausichenko/regextools/NavigationActivity.java
+++ app/src/main/java/com/ausichenko/regextools/NavigationActivity.java
@@ -1,11 +1,9 @@
 package com.ausichenko.regextools;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import com.ausichenko.regextools.fragments.CheatSheetFragment;
 import com.ausichenko.regextools.fragments.MatcherFragment;
@@ -13,38 +11,41 @@ import com.ausichenko.regextools.fragments.PatternsFragment;
 
 public class NavigationActivity extends AppCompatActivity {
 
+    private MatcherFragment mMatcherFragment;
+    private PatternsFragment mPatternsFragment;
+    private CheatSheetFragment mCheatSheetFragment;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            getSupportActionBar().setTitle(item.getTitle());
-            switch (item.getItemId()) {
-                case R.id.nav_matcher:
-                    changeFragment(new MatcherFragment());
-                    return true;
-                case R.id.nav_patterns:
-                    changeFragment(new PatternsFragment());
-                    return true;
-                case R.id.nav_sheet:
-                    changeFragment(new CheatSheetFragment());
-                    return true;
-            }
-            return false;
-        }
-
-    };
+            = item -> {
+                getSupportActionBar().setTitle(item.getTitle());
+                switch (item.getItemId()) {
+                    case R.id.nav_matcher:
+                        changeFragment(mMatcherFragment);
+                        return true;
+                    case R.id.nav_patterns:
+                        changeFragment(mPatternsFragment);
+                        return true;
+                    case R.id.nav_sheet:
+                        changeFragment(mCheatSheetFragment);
+                        return true;
+                }
+                return false;
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
+        mMatcherFragment = new MatcherFragment();
+        mPatternsFragment = new PatternsFragment();
+        mCheatSheetFragment = new CheatSheetFragment();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         getSupportActionBar().setTitle(navigation.getMenu().getItem(0).getTitle());
-        changeFragment(new MatcherFragment());
+        changeFragment(mMatcherFragment);
     }
 
     private void changeFragment(Fragment fragment) {
